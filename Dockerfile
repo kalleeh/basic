9bfef446c5b03e2140db6f4214498fe5a30322d5
kalleh <wallbomk@amazon.com>
2019-01-03 15:34:15 +0100

    write to s3

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,19 @@
-FROM nginx
-COPY index.html /usr/share/nginx/html/index.html
+FROM python:3.6-alpine as base
+
+FROM base as builder
+
+RUN mkdir /install
+WORKDIR /install
+
+COPY requirements.txt /requirements.txt
+
+RUN pip install --install-option="--prefix=/install" -r /requirements.txt
+
+FROM base
+
+COPY --from=builder /install /usr/local
+COPY app /app
+
+WORKDIR /app
+
+CMD ["gunicorn", "-w 2", "-b 0.0.0.0:8000", "app:app"]
---
+++ app/app.py
@@ -0,0 +1,59 @@
+import boto3
+from botocore.exceptions import ValidationError, ClientError
+import logging
+import uuid
+from flask import Flask, render_template
+
+client = boto3.client('s3')
+
+
+def create_temp_file(size, file_name, file_content):
+    random_file_name = ''.join([str(uuid.uuid4().hex[:6]), file_name])
+    with open(random_file_name, 'w') as f:
+        f.write(str(file_content) * size)
+    return random_file_name
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    posts = []
+    user = {'username': 'Karl'}
+
+    first_file_name = create_temp_file(300, 'firstfile.txt', 'f')
+
+    try:
+        # upload file to your bucket
+        client.upload_file(Filename=first_file_name, Bucket='mys3-s3bucket-3alkllybxdoy', Key=first_file_name)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'AlreadyExistsException':
+            app.logger.error('AlreadyExistsException: {}'.format(e))
+            posts.append({
+                'author': {'username': 'Server'},
+                'body': 'Unknown Exception {}'.format(e.text)
+            })
+        else:
+            app.logger.error('Unexpected Error: {}'.format(e))
+            posts.append({
+                'author': {'username': 'Server'},
+                'body': 'Unexpected Error: {}'.format(e.text)
+            })
+    except Exception as e:
+        app.logger.error('Unexpected Error: {}'.format(e))
+        posts.append({
+            'author': {'username': 'Server'},
+            'body': 'Unexpected Error: {}'.format(e)
+        })
+    else:
+        posts.append({
+            'author': {'username': 'Karl'},
+            'body': 'I just wrote a new file named {}'.format(first_file_name)
+        })
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
