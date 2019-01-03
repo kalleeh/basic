7b59146777f8ade2a2dba6ac76d6516a0cfb4eb9
kalleh <wallbomk@amazon.com>
2019-01-03 17:31:50 +0100

    fixed invalid characters

--- app/app.py
+++ app/app.py
@@ -18,8 +18,8 @@ def create_temp_file(size, file_name, file_content):
 app = Flask(__name__)
 
 
-if __name__ != ‘__main__’:
-    gunicorn_logger = logging.getLogger(‘gunicorn.error’)
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
